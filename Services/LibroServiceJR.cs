77e1e10463c52a36a948dae5e8d2078f7703c684
cisco <cisco@UPEAULA-31723>
2024-06-17 11:17:37 -0500

    Agregar archivos de proyecto.

---
+++ Services/LibroServiceJR.cs
@@ -0,0 +1,25 @@
+﻿using MauiApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    internal class LibroServiceJR:ILibroServiceJR
+    {
+        public List<Libro> ObtenerLibro() {
+            return new List<Libro>
+            {
+                new Libro { Titulo = "Libro1", Autor = "Autor1" },
+                   new Libro { Titulo = "Libro2", Autor = "Autor2" },
+                      new Libro { Titulo = "Libro3", Autor = "Autor3" },
+                         new Libro { Titulo = "Libro4", Autor = "Autor4" },
+                            new Libro { Titulo = "Libro5", Autor = "Autor5" },
+            };
+            
+        } }
+}
+   
+
---
+++ ViewModel/LibroViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MauiApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModel
+{
+    public class LibroViewModel:INotifyPropertyChanged
+    {
+        private Libro _libro;
+        public Libro Libro { 
+        get { return _libro; }
+            set { _libro = value;
+                OnPropertyChanged(nameof(Libro));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyname)
+        {
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+    
+}
